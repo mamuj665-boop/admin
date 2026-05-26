1a058a6a680a7d8dd1ae32017aa495c757eb4697
mamuj665-boop <mamuj665@gmail.com>
2026-05-26 05:51:34 -0700

    data

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://shopverse-ef695ae54309.herokuapp.com/api', // Replace with your production API URL
+  stripePublishableKey: 'pk_live_...' // Replace with your Stripe live key from dashboard.stripe.com
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://shopverse-ef695ae54309.herokuapp.com/api',
+  stripePublishableKey: 'pk_test_51ScAUhQQwx1Gjgm789JyGanMftylbUq1rz7FIQT5f0WUD93BmFe2tbzWoNizt6mHGYhqNkx4vBQRnL3ArwnNX9GT00Poxl7234' // Replace with your Stripe test key from dashboard.stripe.com
+};
